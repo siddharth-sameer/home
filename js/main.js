3b3d30572efa10fa26c8dbbbcf60b8e3abce09ff
siddharth-sameer <43922463+siddharth-sameer@users.noreply.github.com>
2018-10-08 00:20:10 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -464,7 +464,7 @@ $(document).ready(function() {
             var mapElement = document.getElementById('map');
             var map = new google.maps.Map(mapElement, mapOptions);
             var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(40.6700, -73.9400),
+                position: new google.maps.(12.973058,77.702884),
                 map: map,
                 title: 'Snazzy!'
             });
